121442fc68975a5787825a5713640f4fc85ce2a6
webdiv-rakib <mdrakibulhassan24@gmail.com>
2024-09-08 02:44:49 +0600

    js function

---
+++ function.js
@@ -0,0 +1,16 @@
+const number = 45;
+
+function fanOffKor() {
+    console.log("Stand up");
+    console.log("Walk toward the switch");
+    console.log("Click the switch to off of the fna");
+}
+
+// function name() {
+// 
+// }
+function brushTeeth() {
+    console.log('pick up the brush');
+    console.log('add paste');
+    console.log('dane bame upore niche ghosho');
+}
