c96dd8f9f8d0ab6da3c92cd64322eed9bd59c627
webdiv-rakib <mdrakibulhassan24@gmail.com>
2024-09-08 03:56:13 +0600

    function parameter example

---
+++ parameter.js
@@ -0,0 +1,33 @@
+function vaatKhao() {
+    console.log('wash your hand first');
+    console.log('seat down');
+    console.log('take meal on your plate');
+    console.log('now eat your meal');
+}
+// vaatKhao();
+// Borgo
+function square(number) {
+    // console.log("Value of the number parameter: ", number);
+    const borgo = number * number;
+    console.log("Square of the given number is: ", borgo);
+}
+square(4);
+console.log("----------------");
+square(5);
+
+// Addition
+function add(num1, num2) {
+    // console.log("Value of the number parameter: ", add);
+    const sum = num1 + num2;
+    console.log("Sum of the given numbers: ", sum);
+}
+add(5, 2);
+console.log("--------------------");
+add(5, 5);
+
+// Subtraction
+function sub(number1, number2) {
+    const subtraction = number1 - number2;
+    console.log("Subtraction of the given number: ", subtraction);
+}
+sub(5, 3);
