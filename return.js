0edd43066fd10dfbae97ed7430bda2489f459964
webdiv-rakib <mdrakibulhassan24@gmail.com>
2024-09-08 20:17:47 +0600

    function return value example

---
+++ return.js
@@ -0,0 +1,10 @@
+function tenTimes(number) {
+    const result = number * 10;
+    return result;
+}
+tenTimes(5);
+const outPut = tenTimes(5);
+console.log('output', outPut);
+
+const bigNumbers = tenTimes(88);
+console.log("Big number: ", bigNumbers);
